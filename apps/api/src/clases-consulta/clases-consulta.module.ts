e33b5113695ff4d7e16f24a1bff3365d50616c3b
Franco Albornoz <francoandresalbornoz2002@gmail.com>
2025-11-11 10:38:33 -0300

    Importando módulo de prisma para que funcionen los servicios.

--- apps/api/src/clases-consulta/clases-consulta.module.ts
+++ apps/api/src/clases-consulta/clases-consulta.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClasesConsultaService } from './services/clases-consulta.service';
 import { ClasesConsultaController } from './controllers/clases-consulta.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ClasesConsultaController],
-  providers: [ClasesConsultaService],
+  providers: [ClasesConsultaService, PrismaService],
 })
 export class ClasesConsultaModule {}
