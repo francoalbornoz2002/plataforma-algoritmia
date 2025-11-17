5bdce471db2ac5920257294aecc9e1980c72ac9c
Franco Albornoz <francoandresalbornoz2002@gmail.com>
2025-11-16 22:17:39 -0300

    Añadiendo nuevo endpoint para registrar o actualizar una o más dificultades de un alumno desde el videojuego.

--- apps/api/src/difficulties/controllers/difficulties.controller.ts
+++ apps/api/src/difficulties/controllers/difficulties.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, ForbiddenException, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  ForbiddenException,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { SubmitDifficultyDto } from '../dto/submit-difficulty.dto';
 import { DifficultiesService } from '../services/difficulties.service';
 import { Public } from 'src/auth/decorators/public.decorator';
@@ -9,18 +16,28 @@ export class DifficultiesController {
 
   /**
    * Endpoint para que el videojuego registre/actualice una dificultad.
+   */
+  // @Public()
+  // @Post('submit-difficulty')
+  // submitDifficulty(@Body() submitDifficultyDto: SubmitDifficultyDto) {
+  //   try {
+  //     return this.difficultiesService.submitDifficulty(submitDifficultyDto);
+  //   } catch (error) {
+  //     if (error instanceof ForbiddenException) {
+  //       throw new ForbiddenException(error.message);
+  //     }
+  //     throw error;
+  //   }
+  // }
+
+  /**
+   * Endpoint para que el videojuego registre/actualice una o varias dificultades.
    * TODO: Proteger esto con un Guardia de API Key.
    */
   @Public()
-  @Post('submit-difficulty')
-  submitDifficulty(@Body() submitDifficultyDto: SubmitDifficultyDto) {
-    try {
-      return this.difficultiesService.submitDifficulty(submitDifficultyDto);
-    } catch (error) {
-      if (error instanceof ForbiddenException) {
-        throw new ForbiddenException(error.message);
-      }
-      throw error;
-    }
+  @Post('submit-difficulties')
+  @HttpCode(HttpStatus.OK)
+  async submitBatchDifficulties(@Body() dtos: SubmitDifficultyDto[]) {
+    return this.difficultiesService.submitDifficulties(dtos);
   }
 }
