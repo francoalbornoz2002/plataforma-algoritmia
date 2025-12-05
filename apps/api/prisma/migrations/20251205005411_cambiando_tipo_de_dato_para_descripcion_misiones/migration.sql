dcfe9b6fe45df5cf368bcc295c7bf1b709b88a58
Franco Albornoz <francoandresalbornoz2002@gmail.com>
2025-12-04 22:05:43 -0300

    Modificando tipo de dato de la descripción para soportar descripciones más largas. Generando la migración.

---
+++ apps/api/prisma/migrations/20251205005411_cambiando_tipo_de_dato_para_descripcion_misiones/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "mision_especial_completada" ALTER COLUMN "descripcion" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "misiones" ALTER COLUMN "descripcion" SET DATA TYPE TEXT;
