857e800889dba80f0e11e4d39a7804fd2b7b0a6c
Franco Albornoz <francoandresalbornoz2002@gmail.com>
2025-12-16 11:54:40 -0300

    Inyectando servicio de sesiones de refuerzo y preguntas al módulo de dificultades (para generar la sesión)

--- apps/api/src/difficulties/difficulties.module.ts
+++ apps/api/src/difficulties/difficulties.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { DifficultiesService } from './services/difficulties.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { DifficultiesController } from './controllers/difficulties.controller';
+import { SesionesRefuerzoService } from 'src/sesiones-refuerzo/service/sesiones-refuerzo.service';
+import { PreguntasService } from 'src/preguntas/services/preguntas.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [DifficultiesService],
+  providers: [DifficultiesService, SesionesRefuerzoService, PreguntasService],
   exports: [DifficultiesService],
   controllers: [DifficultiesController],
 })
--- apps/api/src/preguntas/preguntas.module.ts
+++ apps/api/src/preguntas/preguntas.module.ts
@@ -6,5 +6,6 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Module({
   controllers: [PreguntasController],
   providers: [PreguntasService, PrismaService],
+  exports: [PreguntasService],
 })
 export class PreguntasModule {}
