e9278dc2d4f82444e7cfb9b08c80b8253e3a01df
Franco Albornoz <francoandresalbornoz2002@gmail.com>
2026-01-14 03:03:39 -0300

    Añadiendo campo de ultimo_acceso a la tabla usuarios

---
+++ apps/api/prisma/migrations/20260114050652_anadiendo_campo_ultimo_acceso_a_la_tabla_usuarios/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "usuarios" ADD COLUMN     "ultimo_acceso" TIMESTAMPTZ(6);
--- apps/api/src/users/dto/update-user.dto.ts
+++ apps/api/src/users/dto/update-user.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsDateString, IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsDateString()
+  ultimoAcceso?: string;
+}
