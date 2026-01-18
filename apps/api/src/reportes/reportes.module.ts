dbf383efcab9f0760a9e6fa87f8087da584ab3f5
Franco Albornoz <francoandresalbornoz2002@gmail.com>
2026-01-18 04:05:07 -0300

    Añadiendo dependencia faltante

--- apps/api/src/reportes/reportes.module.ts
+++ apps/api/src/reportes/reportes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ReportesService } from './services/reportes.service';
 import { ReportesController } from './controllers/reportes.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ReportesController],
   providers: [ReportesService],
 })
